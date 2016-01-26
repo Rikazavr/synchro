ec0897792a8c4e86940543fe1efcb90c57504478
Mary <risempai@gmail.com>
2016-01-26 15:13:13 +0000

    old course page

--- assets/js/index.js
+++ assets/js/index.js
@@ -125,22 +125,3 @@ jQuery('#rocketmeluncur').click(function(){
       self.className = 'showrocket';
     },800)
 });
-
-
-//popup subscribe form
-
- $(document).ready(function(event){
-      function loadPopup(event){
-            if($("#popup-overlay").hasClass("popup-hide")){
-                $("#popup-overlay").removeClass("popup-hide");
-            }else{
-                $("#popup-overlay").addClass("popup-show");
-            }
-      }
-       setTimeout(loadPopup, 3000);
-          
-        $("#close").click(function(e){
-            $('#popup-overlay').addClass("popup-hide");
-            e.preventDefault();
-        })
-    });
